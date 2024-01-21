95970bb856ec39003c99b865508c150457f0421c
Srivatsav09 <srivatsav.vijay2003@gmail.com>
2024-01-22 01:02:03 +0530

    feat: signup page created

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50)
+    user_profile = models.ImageField(upload_to="user_profiles/", blank=True, null=True)
+    mobile_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.username
--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
 
 
 def home(request):
     return render(request, "portal/home.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # You can redirect to a success page or perform other actions
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "registration/signup.html", {"form": form})
